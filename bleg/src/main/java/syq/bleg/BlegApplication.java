7b1b83d0eeb7385ed66df03512b6a981ff35aaa9
shiyuquan <550765252@qq.com>
2018-11-14 11:14:18 +0800

    2018年11月14日

--- bleg/src/main/java/syq/bleg/BlegApplication.java
+++ bleg/src/main/java/syq/bleg/BlegApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlegApplication {
 
+    // ces main
     public static void main(String[] args) {
         SpringApplication.run(BlegApplication.class, args);
     }
